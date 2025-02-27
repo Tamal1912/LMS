f1af168f7c386cbae37297bbc2bcda5b65db93e0
Nadia chand mondal <126683993+Nadiachandmondal@users.noreply.github.com>
2025-02-27 21:03:06 +0530

    Update Features.jsx
    rsponsive update

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 
 const FeaturesSection = () => {
   return (
@@ -72,3 +72,4 @@ const FeaturesSection = () => {
 };
 
 export default FeaturesSection;
+ 
